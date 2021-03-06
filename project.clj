12daaa3a94f407143a4d2254763b0b2ea055b9f7
jonathan dannel <usixjad@gmail.com>
2021-03-05 20:16:51 -0500

    init

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject roam-translate "0.1.0-SNAPSHOT"
+  :description "Roam translate API"
+  :url ""
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-http "3.12.0"]
+                 [compojure "1.6.1"]
+                 [environ "1.2.0"]
+                 [org.clojure/data.json "1.0.0"]
+                 [ring/ring-json "0.5.0"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.5"]
+            [lein-environ "1.2.0"]]
+  :ring {:handler translate-roam.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
---
+++ src/roam_translate/handler.clj
@@ -0,0 +1,48 @@
+(ns translate-roam.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [clj-http.client :as client]
+            [environ.core :refer [env]]
+            [clojure.walk :as walk]
+            [clojure.data.json :as json]
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+
+(def apikey (env :apikey))
+(def watson (env :watson))
+
+(defn translate [{:keys [text lang]}]
+  (try
+    (let [resp (client/post
+                watson
+                {:content-type
+                 :json
+                 :basic-auth ["apikey" apikey]
+                 :body (json/write-str {:text text
+                                        :model_id lang})})]
+      ; Success
+      {:success true
+       :result (->
+                (json/read-str (resp :body) :key-fn keyword)
+                :translations
+                first
+                :translation)})
+    ; Error
+    (catch Exception e
+      {:success false
+       :result nil})))
+
+(defroutes app-routes
+  (POST "/translate" {body :body}
+    (response
+     (translate (walk/keywordize-keys body))))
+  (route/not-found nil))
+
+; Wrap app routes in middleware
+(def app
+  (->
+   app-routes
+   (wrap-defaults api-defaults)
+   wrap-json-response
+   wrap-json-body))
